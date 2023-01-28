e4bcc43320b632a1455d48a09a86f179e44d3390
Baimenovbn <baibn@mail.ru>
2023-01-28 20:50:17 +0600

    feat: add faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,9 +1,63 @@
-const FAQ = () => {
+import ExpandableInfo from '../components/mantra/presentational/ExpandableInfo';
+import { Stack } from '@mui/material';
+import Image from 'next/image';
+import styled from '@emotion/styled';
+
+
+const FAQ_LIST = [
+  {
+    title: 'Что такое ноотропы?',
+    text: '',
+  },
+  {
+    title: 'Как принимать ноотропы?',
+    text: '',
+  },
+  {
+    title: 'Легальны ли ноотропы?',
+    text: '',
+  },
+  {
+    title: 'Ноотропы это плацебо?',
+    text: '',
+  },
+  {
+    title: 'Есть ли у ноотропов побочные эффекты?',
+    text: '',
+  },
+  {
+    title: 'Для чего используются ноотропы?',
+    text: '',
+  },
+]
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  position: relative;
+  height: 290px;
+`;
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 100px;
+  
+  max-width: 760px;
+  margin: 0 auto;
+`;
+
+export default function FAQ() {
   return (
-    <h1>
-      FAQ
-    </h1>
+    <Container>
+      <ImageWrapper>
+        <Image layout="fill" src="/assets/mantra/limitless.png" />
+      </ImageWrapper>
+      <Stack width="100%">
+        {
+          FAQ_LIST.map(props => <ExpandableInfo {...props} />)
+        }
+      </Stack>
+    </Container>
   );
 }
-
-export default FAQ;

fb205d724332d4bc594a55aecfc49c41924cfde0
Elmar <e1imar@outlook.com>
2023-10-11 16:27:27 +0600

    config lint fix

--- next.config.js
+++ next.config.js
@@ -3,14 +3,15 @@ module.exports = {
   trailingSlash: true,
   env: {
     // HOST
-    HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
+    // HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
+    // HOST_API_KEY: 'product_site/?&token=2b4a5a62-1238-47f7-9cd3-fdabbba0f172%3ALiYP5ZbIsTuDZe0h4c6VZ5NZFS2ls-wRc89eU0Nvvts',
+    HOST_API_KEY: 'http://bmsys.net:8000',
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'themantra.ru',
-        port: '',
         pathname: '/media/products/**'
       },
     ],
